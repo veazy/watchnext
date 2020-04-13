f0840d2b741354d7d217c2bbc61654b524a4b423
Venkatesh <venky.ch59@gmail.com>
2020-04-13 16:30:50 +0000

    Secured sensitive data

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
+const pwd = require("./pwd");
+var mongopwd = pwd.getPwd();
 mongoose.set('debug', true);
-mongoose.connect('mongodb+srv://venkatesh:Yesmongodb%401710@mycluster-fyzgz.mongodb.net/test?retryWrites=true&w=majority' ,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://venkatesh:' + mongopwd +  '@mycluster-fyzgz.mongodb.net/test?retryWrites=true&w=majority' ,{useNewUrlParser: true, useUnifiedTopology: true});
 //mongodb://localhost:27017/watchnext-api'
 mongoose.Promise = Promise;
 
